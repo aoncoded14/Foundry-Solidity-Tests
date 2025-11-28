672705f2741841c4971fa4ac83cd0590508bfa0e
Daniel Adanyih <dorkuma958@gmail.com>
2025-11-28 19:18:19 +0100

    First Test with Foundry, AttendanceTest and Update and correct the Occasion Contract Code

--- src/OccasionContract.sol
+++ src/OccasionContract.sol
@@ -2,26 +2,25 @@
 pragma solidity >=0.8.2 <0.9.0;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+// import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 error TransferFailed();
 
-contract OccasionContract is ERC20, Ownable {
+contract OccasionContract is ERC20 {
 
-    mapping(address => mapping(address => uint256)) public occ_balances;
+    mapping(address => mapping(address => uint256)) public occBalances;
     
     constructor(uint256 initialSupply, address _creator)
-        ERC20("USDollar-A", "USDA")
-        Ownable(_creator)
-    {
+        ERC20("USDollar-A", "USDA") {
         _mint(msg.sender, initialSupply);
+        _creator = msg.sender;
     }
     
 
     function attendOccasion(uint256 _price, address _token) external returns(bool) {
-        occ_balances[msg.sender] [_token] += _price;
+        occBalances[msg.sender] [_token] += _price;
         bool success = ERC20(_token).transferFrom(msg.sender, address(this), _price);
-        if(!success) revert TransferFailed;
+        if(!success) revert TransferFailed();
         return success;
     }
 
--- test/OccasionContract.t.sol
+++ test/OccasionContract.t.sol
@@ -2,3 +2,24 @@
 pragma solidity ^0.8.13;
 
 import {Test} from "forge-std/Test.sol";
+import {OccasionContract} from "../src/OccasionContract.sol";
+import {AonERC20} from "./mocks/AonERC20.sol";
+
+contract OccasionContractTest is Test {
+    OccasionContract public occasion;
+    AonERC20 public aonToken;
+    address public creator = address(this);
+
+    function setUp() public {
+        uint256 initialSupply = 1000e18; 
+        occasion = new OccasionContract(initialSupply, creator);
+        aonToken = new AonERC20();
+    }
+
+    function attendanceTest() public {
+        uint256 amount = 10e18;
+        aonToken.approve(address(occasion), amount);
+        bool attendancePassed = occasion.attendOccasion(amount, address(aonToken));
+        assertTrue(attendancePassed);
+    }
+}
